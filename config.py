eb7a10dc86577614601e630096a064e2231a50b8
your_github_username <your_github_email>
2024-11-22 19:34:23 +0400

    with config.ini

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 from configparser import ConfigParser
-from enum import Enum
+
 
 config_filename = 'config.ini'
-
 parser = ConfigParser()
 
 
@@ -12,22 +11,6 @@ parser = ConfigParser()
 Модуль для проверки и получения конфигурационных файлов
 
 """
-
-
-
-def get_db_params(filename=config_filename, section='postgresql'):
-    parser.read(filename, "utf-8")
-
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Ошибка подключения к БД! Секция {0} не найдена в файле {1}'.format(section, filename))
-
-    
-    return db
 
 
 
@@ -62,14 +45,3 @@ def get_feedback_chat_id(filename=config_filename, section='telegram'):
     return chat_id
 
 
-
-
-def get_frequency_spam(filename=config_filename, section='spam'):
-    parser.read(filename, "utf-8")
-
-    if parser.has_section(section):
-        chat_id = parser.get(section, 'frequency')
-    else:
-        raise Exception('Ошибка настройки обратной связи! Секция {0} не найдена в файле {1}'.format(section, filename))
-
-    return chat_id
